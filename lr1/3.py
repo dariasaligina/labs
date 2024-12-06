a1729b2abadde3a878e73f7e1a06133adec2357a
dariasaligina <146203741+dariasaligina@users.noreply.github.com>
2024-12-06 19:49:36 +0300

    Update 3.py

--- lr1/3.py
+++ lr1/3.py
@@ -2,7 +2,7 @@ import sys
 import math
 
 try:
-    number = float(sys.stdin.read().strip())
+    number = float(sys.stdin.readline().strip())
     result = math.sqrt(number)
     with open('output.txt', 'w') as f:
         print(result, file=f)
