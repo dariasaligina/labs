0aae6c904ad8bb1e78718e455aa43171e168d850
dariasaligina <146203741+dariasaligina@users.noreply.github.com>
2024-12-11 19:34:50 +0300

    Update greter.py

--- lr2/greter.py
+++ lr2/greter.py
@@ -2,7 +2,7 @@ import re
 
 
 
-def is_valid_name(name):
+def is_valid(name):
     if not name or name[0].islower():
         return False
     if not re.match("^[A-Za-zа-яёА-ЯЁ]+$", name):
@@ -13,26 +13,31 @@ def is_valid_name(name):
 def main():
     error_file = 'error.txt'
     try:
-        with open('names.txt', 'r', encoding='utf-8') as f:
-            names = f.read().splitlines()
+        try:
+            with open('names.txt', 'r', encoding='utf-8') as f:
+                names = f.read().splitlines()
 
-        for name in names:
-            if is_valid_name(name):
-                print(f"Привет, {name}!")
-            else:
-                with open(error_file, 'a', encoding='utf-8') as ef:
-                    ef.write(f"Ошибка: недопустимое имя '{name}'\n")
+            for name in names:
+                if is_valid(name):
+                    print(f"Привет, {name}!")
+                else:
+                    with open(error_file, 'a', encoding='utf-8') as ef:
+                        ef.write(f"Ошибка: недопустимое имя '{name}'\n")
+        except:
+            try:
+                while True:
+                    name = input("Пожалуйста, введите ваше имя: ").strip()
+                    if is_valid(name):
+                        print(f"Привет, {name}!")
+                    else:
+                        with open(error_file, 'a', encoding='utf-8') as ef:
+                            ef.write(f"Ошибка: недопустимое имя '{name}'\n")
+            except KeyboardInterrupt:
+                    print("\nGoodbye!")
 
-        while True:
-            name = input("Пожалуйста, введите ваше имя: ").strip()
-            if is_valid_name(name):
-                print(f"Привет, {name}!")
-            else:
-                with open(error_file, 'a', encoding='utf-8') as ef:
-                    ef.write(f"Ошибка: недопустимое имя '{name}'\n")
 
     except Exception as e:
-        with open(error_file, 'a') as ef:
+        with open(error_file, 'a', encoding='utf-8') as ef:
             ef.write(f"Ошибка: {e}\n")
 
 
